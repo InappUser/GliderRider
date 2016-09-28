46e4d2385973608ad5ea7bbb011910d1a80ef440
TomDixon <tom.-.dixon@live.co.uk>
2016-09-28 15:50:42 +0100

    -Added the player Game Object -Created rudimentary Player movement and rotation script (FlightControl.cs) -Added model to Player -Attached main camera to Player -Added mesh collider (convex) to Player -Added Ground object -Added box collider to ground

---
+++ Assets/Scripts/FlightControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlightControl : MonoBehaviour {
+    float movementSpeed = 1000f;
+    float rotSpeed = 20f;
+    float vertMovement;
+    float horiMovement;
+    Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+        rigidBody = this.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Quaternion rotToAdd = Quaternion.identity;
+
+        horiMovement = Input.GetAxisRaw("Roll") * (Time.deltaTime * rotSpeed);
+        vertMovement = Input.GetAxisRaw("Pitch") * (Time.deltaTime * rotSpeed);
+
+        rotToAdd.eulerAngles =new  Vector3(vertMovement, horiMovement, 0);
+        rigidBody.rotation *= rotToAdd;
+
+
+        Vector3 posToAdd = Vector3.forward;
+        posToAdd = rigidBody.rotation * posToAdd;
+        rigidBody.velocity = posToAdd * (Time.deltaTime * movementSpeed);
+
+    }
+}
