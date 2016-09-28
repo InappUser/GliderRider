994c6ab8ac9f10dbead04b1285f273539d389275
TomDixon <tom.-.dixon@live.co.uk>
2016-09-28 20:00:11 +0100

    -another commit due to issues with git -Adding Glider script and third person camera script

---
+++ Assets/Scripts/Glider.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glider : MonoBehaviour {
+    public GameObject gliderCamera;
+
+
+    Vector3 initCameraDistance;
+    float movementSpeed = 1000f;
+    float startAccelerationSpeed = 1;
+    float currentAccelerationSpeed = 1;
+    float vertRotSpeed = 20f;
+    float horiRotSpeed = 40f;
+
+
+    float rollToYawMultiplier = .2f; 
+
+    float recentreSpeed = .2f;
+    //Quaternion CentredRotation;
+    Rigidbody myRigidbody;
+
+
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = transform.GetComponent<Rigidbody>();
+        //CentredRotation = Quaternion.identity
+        initCameraDistance = this.transform.position - gliderCamera.transform.position;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+       
+       // CameraMovement();
+        //print(transform.rotation);
+        if (Input.GetKey(KeyCode.Space)) {
+            Recentre();
+            //print("keydown");
+        }
+        Quaternion rotToAdd = Quaternion.identity; //resetting physics frame's rotation
+
+        float pitch;
+        float roll;
+        float yaw;
+
+        pitch = Input.GetAxisRaw("Vertical") * (Time.deltaTime * vertRotSpeed);
+        yaw = 0;//(Input.GetAxisRaw("Horizontal") * (Time.deltaTime * horiRotSpeed));// * rollToYawMultiplier;
+        roll = -(Input.GetAxisRaw("Horizontal") * (Time.deltaTime * horiRotSpeed)); //updating the roll and pitch rotaiton (y and x rotaiton, respectively)
+        print("pitch " + pitch);
+
+
+        //print(
+        //        "pitch " + pitch
+        //        + ", yaw " +yaw 
+        //        + ", roll " + roll
+        //    );
+        print("rigidbody rot: " + myRigidbody.rotation.eulerAngles);
+        rotToAdd.eulerAngles = new  Vector3(pitch, yaw, roll);
+        myRigidbody.rotation *= rotToAdd;
+
+        //if (myRigidbody.rotation.eulerAngles.x < 290 && myRigidbody.rotation.eulerAngles.x > 100)
+        //{
+        //    startAccelerationSpeed -= 0.1f;
+        //}
+        //else {
+        //    startAccelerationSpeed += 0.1f;
+        //}
+
+        //startAccelerationSpeed = Mathf.Clamp(startAccelerationSpeed, 0.1f, 1.9f);
+
+        //movementSpeed *= startAccelerationSpeed;
+
+
+        Vector3 posToAdd = Vector3.forward;
+        posToAdd = myRigidbody.rotation * posToAdd;
+        myRigidbody.velocity = posToAdd * (Time.deltaTime * movementSpeed);
+
+    }
+
+    void Recentre()
+    {
+        Quaternion zeroPitchRoll = Quaternion.Euler(0, transform.rotation.y, 0);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.identity, recentreSpeed);
+
+    }
+
+    void CameraMovement() {
+        gliderCamera.transform.position = this.transform.position - initCameraDistance; //- gliderCamera.transform.position; 
+        //get difference between cam pos and glider pos
+        //make current cam pos glider pos - difference
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public Transform lookAt;
+    public Transform camTransform;
+    Transform posCheck;
+    public float sensX = 4.0f, sensY = 1.0f, sensScroll = 1.5f;
+    public float xAxisStarRot = 30.0f, distance = 10.0f;
+
+    private Camera cam;
+
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+    private float currentX = 0;
+    private float currentY = 0;
+
+
+    private Vector3 lastEuler;
+    private float eulerDelta;
+
+    private void Start()
+    {
+        currentY = xAxisStarRot;
+        //		Cursor.lockState = CursorLockMode.Locked;
+        //		Cursor.visible = false;
+        lastEuler = lookAt.eulerAngles;
+        camTransform = transform;
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        //		print ("delta "+eulerDelta);
+        //		print ("mousex " + Input.GetAxis ("Mouse X"));
+        currentX += /*eulerDelta  +*/ Input.GetAxis("Mouse X") * sensX;
+        currentY += -Input.GetAxis("Mouse Y") * sensY;
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        GetEulerDelta();
+    }
+    private void LateUpdate()
+    {//doing in the late update because the normal update will be used for player movement - this is dependant on that
+        distance -= Input.GetAxis("Mouse ScrollWheel") * sensScroll;
+        Vector3 dir = new Vector3(0, 0, -distance);
+        //Quaternion lkAt = Quaternion.Euler (0,lookAt.rotation.y,0);//lookAt.rotation;
+
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        //posCheck.position = lookAt.position + rotation * dir;
+        //Vector3 posCheckDir = (camTransform.position - posCheck.position).normalized;
+        //if(Physics.Raycast(camTransform.position,posCheckDir
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+
+    }
+    void GetEulerDelta()
+    {
+        eulerDelta = lookAt.eulerAngles.y - lastEuler.y;
+        lastEuler = lookAt.eulerAngles;
+    }
+}
