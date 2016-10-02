c034d6d1373cbc213d1214dcfef30399051ea392
Tom Dixon <tom.-.dixon@live.co.uk>
2016-10-02 01:49:36 +0100

    -Added collectibles grouping: a parent object holds a set of collectibles to be enabled. -Made it so a random collectible group will be enabled, while all others are disabled.
    Note: realised a potential flaw with the collectibles in that changing player speed may negatively affect their ability to collide with other collectibles

--- Assets/Scripts/Run.cs
+++ Assets/Scripts/Run.cs
@@ -1,18 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Run : MonoBehaviour {
     public Text scoreText;
+    public GameObject collectiblesParent;
 
-    int score;
-    int fuelCanisters;
+    private GameObject[] collectibleSpawnGroups;
+    private int score;
+    private int fuelCanisters;
     // Use this for initialization
+    void Start() {
+        print("cc:" + collectiblesParent.transform.childCount);
+        collectibleSpawnGroups = GameObject.FindGameObjectsWithTag("CollectibleGroup");
+        foreach (GameObject go in collectibleSpawnGroups) { //dissable all groups before enableing the one wanted
+            go.SetActive(false);
+        }
+        SpawnCollectibles();
+    }
     public void FuelCanisterCollected()
     {
         fuelCanisters++;
         scoreText.text = (fuelCanisters * 10).ToString();
     }
+
+    void SpawnCollectibles() {
+        if (collectibleSpawnGroups.Length < 1) return;
+        int groupToSpawn = Random.Range(0, collectibleSpawnGroups.Length);
+        collectibleSpawnGroups[groupToSpawn].SetActive(true);
+
+    }
+
 
 
 }
